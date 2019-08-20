0eee94b7121fd36182be281b18fffff31750841c
Priscilla Firpo <priscillayf@gmail.com>
2019-08-20 04:45:12 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -16,8 +16,6 @@ def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language_optional
-
 
 
 
